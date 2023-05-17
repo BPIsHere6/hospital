e71c20063b51e185dc1ef248ef6236380658ccd7
BP0928 <863752311@qq.com>
2023-05-17 21:58:39 +0800

    cancer chart back

--- back/src/main/java/cn/pan/doctor/controller/ChartController.java
+++ back/src/main/java/cn/pan/doctor/controller/ChartController.java
@@ -3,13 +3,13 @@ package cn.pan.doctor.controller;
 import cn.pan.basics.baseVo.Result;
 import cn.pan.basics.utils.ResultUtil;
 import cn.pan.doctor.entity.CancerData;
-import cn.pan.doctor.entity.Doctor;
 import cn.pan.doctor.service.CancerDataService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,8 +21,25 @@ public class ChartController {
 
     @RequestMapping("/CancerData")
     @ApiOperation(value = "查询癌症数据")
-    public Result<List<CancerData>> getCancerData(){
-        return new ResultUtil<List<CancerData>>().setData(cancerDataService.list(null));
+    public Result<List<Object>> getCancerData(){
+
+        List<CancerData> data = cancerDataService.list(null);
+        System.out.println("********************");
+        System.out.println(data);
+        List<Object> response = new ArrayList<>();
+        String[] head = new String[]{"Income","Life Expectancy","Population","Country","Year"};
+        response.add(head);
+        for (CancerData c:data) {
+            List<Object> newList = new ArrayList<>();
+            newList.add(c.getPatientsNumber());
+            newList.add(c.getExpectationLife());
+            newList.add(c.getPopulation());
+            newList.add(c.getCountry());
+            newList.add(c.getYear());
+            response.add(newList);
+        }
+        System.out.println(response);
+        return new ResultUtil<List<Object>>().setData(response);
     }
 
 }
